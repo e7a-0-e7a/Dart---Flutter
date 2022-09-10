24d9ec163c6f9c24fdcb3ce31d3b93f5c4f1acfa
e7a-0-e7a <m.grabke@trikru.de>
2022-09-10 20:48:52 +0200

    add macos

--- flutter_grundlagen/ios/Flutter/flutter_export_environment.sh
+++ flutter_grundlagen/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\tools\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Rhaven\Codes\Eigene\Kurse\Android\Dart + Flutter\flutter_grundlagen"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.3.1/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rhaven/Documents/Kurse/Flutter--Dart/Dart---Flutter/flutter_grundlagen"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
